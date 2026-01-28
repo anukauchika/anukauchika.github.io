514b63e7ab086b0ea2e9a71ffebf9066eda0821e
Sherzod Gapirov <sh.gapirov@gmail.com>
2026-01-28 22:49:09 +0600

    Add g-analytics.

--- web/src/practice.js
+++ web/src/practice.js
@@ -1,6 +1,9 @@
 import { mount } from 'svelte'
 import './practice.css'
 import Practice from './Practice.svelte'
+import { initAnalytics } from './utils/analytics.js'
+
+initAnalytics()
 
 mount(Practice, {
   target: document.getElementById('app'),
--- web/src/workbook.js
+++ web/src/workbook.js
@@ -1,6 +1,9 @@
 import { mount } from 'svelte'
 import './workbook.css'
 import Workbook from './Workbook.svelte'
+import { initAnalytics } from './utils/analytics.js'
+
+initAnalytics()
 
 mount(Workbook, {
   target: document.getElementById('app'),
